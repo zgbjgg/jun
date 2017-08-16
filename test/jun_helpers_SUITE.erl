4cb5bb5ab1293bc0d03cf5eddf96e0501e905a94
zgbjgg <zgbjgg@gmail.com>
2017-08-16 12:35:11 -0500

    add tests for len_columns, len_index and memory_usage in jun helpers

--- test/jun_helpers_SUITE.erl
+++ test/jun_helpers_SUITE.erl
@@ -4,10 +4,16 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -export([all/0, init_per_testcase/2, end_per_testcase/2]).
--export([test_jun_pandas_columns/1]).
+-export([test_jun_pandas_columns/1,
+    test_jun_pandas_len_columns/1,
+    test_jun_pandas_len_index/1,
+    test_jun_pandas_memory_usage/1]).
 
 all() ->
-    [test_jun_pandas_columns].
+    [test_jun_pandas_columns,
+     test_jun_pandas_len_columns,
+     test_jun_pandas_len_index,
+     test_jun_pandas_memory_usage].
 
 init_per_testcase(_, _Config) ->
     % for each case start a new worker
@@ -26,3 +32,21 @@ test_jun_pandas_columns([{jun_worker, Pid}, {path, Path}, _]) ->
     {ok, Columns} = jun_pandas:columns(Pid, DataFrame, []),
     Out = <<"name,age">>,
     ?assertEqual(Out, Columns).
+
+test_jun_pandas_len_columns([{jun_worker, Pid}, {path, Path}, _]) ->
+    {ok, DataFrame} = jun_pandas:read_csv(Pid, Path),
+    {ok, LenColumns} = jun_pandas:len_columns(Pid, DataFrame, []),
+    Out = 2,
+    ?assertEqual(Out, LenColumns).
+
+test_jun_pandas_len_index([{jun_worker, Pid}, {path, Path}, _]) ->
+    {ok, DataFrame} = jun_pandas:read_csv(Pid, Path),
+    {ok, LenIndex} = jun_pandas:len_index(Pid, DataFrame, []),
+    Out = 6,
+    ?assertEqual(Out, LenIndex).
+
+test_jun_pandas_memory_usage([{jun_worker, Pid}, {path, Path}, _]) ->
+    {ok, DataFrame} = jun_pandas:read_csv(Pid, Path),
+    {ok, MemoryUsage} = jun_pandas:memory_usage(Pid, DataFrame, []),
+    Out = <<"425.0+ bytes">>,
+    ?assertEqual(Out, MemoryUsage).
