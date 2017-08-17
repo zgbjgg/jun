77cf9899157734caca0b9350d5e22f41c656c80b
zgbjgg <zgbjgg@gmail.com>
2017-08-17 11:19:47 -0500

    add plotting test suite

---
+++ test/jun_plotting_SUITE.erl
@@ -0,0 +1,28 @@
+-module(jun_plotting_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-export([all/0, init_per_testcase/2, end_per_testcase/2]).
+-export([test_jun_pandas_plot/1]).
+
+all() ->
+    [test_jun_pandas_plot].
+
+init_per_testcase(_, _Config) ->
+    % for each case start a new worker
+    {ok, Pid} = jun_worker:start_link(),
+    % load the default file to execute tests
+    {ok, Cwd} = file:get_cwd(), 
+    Path = list_to_atom(Cwd ++ "/../../lib/jun/test/files/csv_plot.txt"),
+    [{jun_worker, Pid}, {path, Path}, {cwd, Cwd}].
+
+end_per_testcase(_, _Config) ->
+    % @todo stop the worker
+    ok.
+
+test_jun_pandas_plot([{jun_worker, Pid}, {path, Path}, _]) ->
+    {ok, DataFrame} = jun_pandas:read_csv(Pid, Path),
+    Plot = jun_pandas:plot(Pid, DataFrame, 'fig.png', [{'kind', 'line'},
+        {'x', 'name'}, {'y', 'age'}]),
+    ?assertEqual(Plot, {ok, <<"done">>}).
