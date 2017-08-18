6892bd3ca2f9c2646a9f08b2572c8a97ebc95e2d
zgbjgg <zgbjgg@gmail.com>
2017-08-18 12:10:00 -0500

    dataframe as tuple in jun py encoder

--- priv/jun_dataframe.py
+++ priv/jun_dataframe.py
@@ -12,5 +12,7 @@ def dtype_encoder(value):
         return np.asscalar(value)
     elif isinstance(value, np.float64):
         return np.asscalar(value)
+    elif isinstance(value, pd.core.frame.DataFrame):
+        return (Atom("pandas.core.frame.DataFrame"), value)
     else:
         return value
