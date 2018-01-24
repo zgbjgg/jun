e72b25bd3d3bc9bc0d3f1dcf6480d00e2fdee960
zgbjgg <zgbjgg@gmail.com>
2018-01-24 17:27:28 -0600

    add full support for all categorical plots using seaborn

--- src/jun_seaborn.erl
+++ src/jun_seaborn.erl
@@ -9,7 +9,15 @@
 -export([lmplot/4,
     factorplot/4,
     pairplot/4,
-    jointplot/4]).
+    jointplot/4,
+    stripplot/4,
+    swarmplot/4,
+    boxplot/4,
+    violinplot/4,
+    lvplot/4,
+    pointplot/4,
+    barplot/4,
+    countplot/4]).
 
 %% Axis grids
 
@@ -29,8 +37,41 @@ jointplot(Pid, DataFrame, Filename, Keywords) ->
     gen_server:call(Pid, {'core.jun.dataframe.seaborn', [DataFrame, 'jointplot',
         Filename, [{'data', DataFrame} | Keywords]]}, infinity).
 
+%% Categorical plots
+
+stripplot(Pid, DataFrame, Filename, Keywords) ->
+    gen_server:call(Pid, {'core.jun.dataframe.seaborn', [DataFrame, 'stripplot',
+        Filename, [{'data', DataFrame} | Keywords]]}, infinity).
+
+swarmplot(Pid, DataFrame, Filename, Keywords) ->
+    gen_server:call(Pid, {'core.jun.dataframe.seaborn', [DataFrame, 'swarmplot',
+        Filename, [{'data', DataFrame} | Keywords]]}, infinity).
+
+boxplot(Pid, DataFrame, Filename, Keywords) ->
+    gen_server:call(Pid, {'core.jun.dataframe.seaborn', [DataFrame, 'boxplot',
+        Filename, [{'data', DataFrame} | Keywords]]}, infinity).
+
+violinplot(Pid, DataFrame, Filename, Keywords) ->
+    gen_server:call(Pid, {'core.jun.dataframe.seaborn', [DataFrame, 'violinplot',
+        Filename, [{'data', DataFrame} | Keywords]]}, infinity).
+
+lvplot(Pid, DataFrame, Filename, Keywords) ->
+    gen_server:call(Pid, {'core.jun.dataframe.seaborn', [DataFrame, 'lvplot',
+        Filename, [{'data', DataFrame} | Keywords]]}, infinity).
+
+pointplot(Pid, DataFrame, Filename, Keywords) ->
+    gen_server:call(Pid, {'core.jun.dataframe.seaborn', [DataFrame, 'pointplot',
+        Filename, [{'data', DataFrame} | Keywords]]}, infinity).
+
+barplot(Pid, DataFrame, Filename, Keywords) ->
+    gen_server:call(Pid, {'core.jun.dataframe.seaborn', [DataFrame, 'barplot',
+        Filename, [{'data', DataFrame} | Keywords]]}, infinity).
+
+countplot(Pid, DataFrame, Filename, Keywords) ->
+    gen_server:call(Pid, {'core.jun.dataframe.seaborn', [DataFrame, 'countplot',
+        Filename, [{'data', DataFrame} | Keywords]]}, infinity).
+
 %% @TODO:
-%% * Categorical plots
 %% * Distribution plots
 %% * Regression plots
 %% * Matrix plots
