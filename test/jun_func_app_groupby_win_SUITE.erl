1b27a85eaee3a83aced53fd6ee528372fa36b591
zgbjgg <zgbjgg@gmail.com>
2018-02-23 15:29:48 -0600

    add tests for apply using a single lambda function

--- test/jun_func_app_groupby_win_SUITE.erl
+++ test/jun_func_app_groupby_win_SUITE.erl
@@ -5,12 +5,16 @@
 
 -define(DATAFRAME, 'pandas.core.frame.DataFrame').
 -define(GROUPBY, 'pandas.core.groupby.DataFrameGroupBy').
+-define(SERIES, 'pandas.core.frame.Series').
+-define(LAMBDA, <<"lambda row : row['age'] + 7">>).
 
 -export([all/0, init_per_testcase/2, end_per_testcase/2]).
--export([test_jun_pandas_groupby/1]).
+-export([test_jun_pandas_groupby/1,
+    test_jun_pandas_apply/1]).
 
 all() ->
-    [test_jun_pandas_groupby].
+    [test_jun_pandas_groupby,
+     test_jun_pandas_apply].
 
 init_per_testcase(_, _Config) ->
     % for each case start a new worker
@@ -28,3 +32,9 @@ test_jun_pandas_groupby([{jun_worker, Pid}, {path, Path}, _]) ->
     {ok, {?DATAFRAME, DataFrame}} = jun_pandas:read_csv(Pid, Path),
     {ok, Group} = jun_pandas:groupby(Pid, DataFrame, 'name', []),
     ?assertMatch({?GROUPBY, {'$erlport.opaque', python, _}}, Group).
+
+test_jun_pandas_apply([{jun_worker, Pid}, {path, Path}, _]) ->
+    {ok, {?DATAFRAME, DataFrame}} = jun_pandas:read_csv(Pid, Path),
+    {ok, Series} = jun_pandas:apply(Pid, DataFrame, 'None', [{lambda, ?LAMBDA},
+        {axis, 1}]),
+    ?assertMatch({?SERIES, {'$erlport.opaque', python, _}}, Series).
