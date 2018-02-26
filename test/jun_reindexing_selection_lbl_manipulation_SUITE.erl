6cafb923efb1c6300962f4c91c10ec55875d817b
zgbjgg <zgbjgg@gmail.com>
2018-02-26 17:18:18 -0600

    add suite test for reindexing methods, for now add drop

---
+++ test/jun_reindexing_selection_lbl_manipulation_SUITE.erl
@@ -0,0 +1,32 @@
+-module(jun_reindexing_selection_lbl_manipulation_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-define(DATAFRAME, 'pandas.core.frame.DataFrame').
+
+-export([all/0, init_per_testcase/2, end_per_testcase/2]).
+-export([test_jun_pandas_drop/1]).
+
+all() ->
+    [test_jun_pandas_drop].
+
+init_per_testcase(_, _Config) ->
+    % for each case start a new worker
+    {ok, Pid} = jun_worker:start_link(),
+    % load the default file to execute tests
+    {ok, Cwd} = file:get_cwd(),
+    Path = list_to_atom(Cwd ++ "/../../lib/jun/test/files/csv.txt"),
+    [{jun_worker, Pid}, {path, Path}, {cwd, Cwd}].
+
+end_per_testcase(_, _Config) ->
+    % @todo stop the worker
+    ok.
+
+test_jun_pandas_drop([{jun_worker, Pid}, {path, Path}, _]) ->
+    {ok, {?DATAFRAME, DataFrame}} = jun_pandas:read_csv(Pid, Path), 
+    {ok, {?DATAFRAME, DropDataFrame}} = jun_pandas:drop(Pid, DataFrame, 'age', [{'axis', 1}]),
+    {ok, Erl} = jun_pandas:to_erl(Pid, DropDataFrame),
+    ?assertEqual({'pandas.core.frame.DataFrame', [<<"name">>], [[<<"Allison">>],
+        [<<"George">>], [<<"Kristen">>], [<<"Debbie">>], [<<"Bjork">>],
+        [<<"Katy">>]]}, Erl).
