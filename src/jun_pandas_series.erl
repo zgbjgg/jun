5a2e00ab37a12e33f51863cb16635b1c1b3f7098
zgbjgg <zgbjgg@gmail.com>
2018-04-06 10:45:16 -0500

    add new module to work with pandas series

---
+++ src/jun_pandas_series.erl
@@ -0,0 +1,16 @@
+%%
+%% This module is a wrapper for some functions in pandas used in Series,
+%% using a single pid that holds a dataframe.
+%% The pid is an instance of the jun_py_worker module and is capable
+%% to execute commands over py interface, keeping the tracking of all transactions
+%%
+-module(jun_pandas_series).
+
+-export([combine/4]).
+
+%% Binary operator functions
+
+combine(Pid, Series, Series2, Keywords) ->
+    Keywords0 = proplists:delete(func, Keywords),
+    Func = proplists:get_value(func, Keywords),
+    gen_server:call(Pid, {'core.jun.series', [Series, combine, [Series2, Func], 'None', Keywords0]}, infinity).
